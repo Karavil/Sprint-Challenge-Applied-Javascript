b470879159b5b9aae2c125e5965a322d4e374faf
Alex Karavil <alex@karavil.io>
2020-02-14 11:28:42 -0500

    Done with fetching topics from back-end API

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+function createTopic(topicName) {
+   const topicTab = document.createElement('div');
+   topicTab.classList.add('tab');
+   topicTab.textContent = topicName;
+
+   return topicTab;
+}
+
+const LAMBDA_API = 'https://lambda-times-backend.herokuapp.com/topics';
+const topics = document.querySelector('.topics');
+
+// Request all available topics, then create a topic div for each one
+axios.get(LAMBDA_API)
+   .then(function (response) {
+      response.data.topics.forEach(topic => {
+         // Create DOM element
+         const newTopic = createTopic(topic);
+         // Append element to div with .topics class specified above
+         topics.appendChild(newTopic)
+      });
+   })
+   .catch(function (error) {
+      // handle error
+      console.log(error);
+   })
